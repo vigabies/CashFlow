f9444553c5599619e292eb223f1adfb85c784b7d
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-09 11:00:59 -0300

    Validando despesas usando if

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
+using System.Data;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 
@@ -8,8 +9,29 @@ public class RegisterExpenseUseCase
     public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
     {
 
-        //fazer as validações ainda
+        Validate(request);
 
         return new ResponseRegisteredExpenseJson;
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty) {
+            throw new ArgumentException("The title is required.");
+        
+        }
+
+        if(request.Amount <= 0)
+        {
+            throw new ArgumentException("The Amoumnt must be greater than zero.");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result < 0) {
+            throw new ArgumentException("Expenses cannot be for the future");
+        }
+
+
     }
 }
--- src/CashFlow.Communication/Requests/RequestExpenseJson.cs
+++ src/CashFlow.Communication/Requests/RequestExpenseJson.cs
@@ -5,9 +5,9 @@ namespace CashFlow.Communication.Requests;
 public class RequestRegisterExpenseJson
 {
     public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty; //opcional
 
-    public DateTime Date {  get; set; }
+    public DateTime Date {  get; set; } //não posso receber data do futuro
 
     public decimal Amount { get; set; }
 
