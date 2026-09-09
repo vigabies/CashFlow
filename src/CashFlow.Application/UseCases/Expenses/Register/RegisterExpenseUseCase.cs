5da3be4a21e5481102dee67e5401a90ab4fee1fd
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-09 14:12:01 -0300

    FluentValidation p/ validação em classes
     (tipo IF)

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,7 +1,5 @@
-﻿using CashFlow.Communication.Enums;
-using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
-using System.Data;
 
 namespace CashFlow.Application.UseCases.Expenses.Register;
 
@@ -17,27 +15,8 @@ public class RegisterExpenseUseCase
 
     private void Validate(RequestRegisterExpenseJson request)
     {
-        var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-        if (titleIsEmpty) {
-            throw new ArgumentException("The title is required.");
-        
-        }
+        var validator = new RegisterExpenseValidator();
 
-        if(request.Amount <= 0)
-        {
-            throw new ArgumentException("The Amoumnt must be greater than zero.");
-        }
-
-        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-        if (result < 0) {
-            throw new ArgumentException("Expenses cannot be for the future");
-        }
-
-        var paymenteTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-        if (paymenteTypeIsValid ==  false)
-        {
-            throw new ArgumentException("Paymente Type isn't valid");
-        }
-                
+        var result = validator.Validate(request);
     }
 }
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,16 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
+{
+    public RegisterExpenseValidator()
+    {
+        RuleFor(expense => expense.Title).NotEmpty().WithMessage("The title is required."); //criando uma regra
+        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("The Amoumnt must be greater than zero.");
+        RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Expenses cannot be for the future");
+        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage("Paymente Type isn't valid");
+    }
+
+}
