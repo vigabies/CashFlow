778093e0bb214b5c4b4d2eb59c2737240c13d9ae
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-09 11:14:49 -0300

    Try Catch

--- src/CashFlow.Api/Controllers/ExpensesController.cs
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -12,10 +12,22 @@ public class ExpensesController : ControllerBase
     [HttpPost]
     public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
     {
-        var useCase = new RegisterExpenseUseCase();
+        try
+        {
+            var useCase = new RegisterExpenseUseCase();
 
-        var response = useCase.Execute(request);
-        
-        return Created(string.Empty, response);
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+        catch(ArgumentException ex)
+        {
+            return BadRequest(ex.Message);
+        }
+
+        catch
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, "unknow error");
+        }
     }
 }
