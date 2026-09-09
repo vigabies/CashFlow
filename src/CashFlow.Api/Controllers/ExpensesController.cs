e351153b3886126393927a7ab33d1f6c65b69d7c
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-09 10:27:51 -0300

    Criando o endpoint pra salvar despesa

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CashFlow.Communication.Requests;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        return Created();
+    }
+}
