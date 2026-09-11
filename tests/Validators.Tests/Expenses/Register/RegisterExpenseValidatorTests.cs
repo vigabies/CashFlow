9c6c13ea31b3593bef7ea8ab3ea4b4d2d3640f3b
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-11 14:58:10 -0300

    + Bogus

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
 using CashFlow.Communication.Requests;
+using CommomTestUtilities.Requests;
 
 namespace Validators.Tests.Expenses.Register;
 
@@ -10,15 +11,8 @@ public class RegisterExpenseValidatorTests
     {
         //Arrange
         var validator = new RegisterExpenseValidator();
-        var request = new RequestRegisterExpenseJson
-        {
-            Amount = 100,
-            Date = DateTime.Now.AddDays(-1),
-            Description = "Description",
-            Title = "Title",
-            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard
-        };
-
+        var request = RequestRegisterExpenseJsonBuilder.Build(); 
+       
         //Act
         var result = validator.Validate(request);
 
