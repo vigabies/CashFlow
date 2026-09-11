f90bc3a07f7efcc139854a24473cb49e32d11228
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-11 10:49:50 -0300

    Middleware para idiomas

---
+++ src/CashFlow.Api/Middleware/CultureMiddleware.cs
@@ -0,0 +1,15 @@
+﻿namespace CashFlow.Api.Middleware;
+
+public class CultureMiddleware
+{
+    private readonly RequestDelegate _next;
+    public CultureMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task invoke(HttpContext context)
+    {
+
+    }
+}
