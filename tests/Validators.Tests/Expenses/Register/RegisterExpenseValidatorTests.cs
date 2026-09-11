cc9ad013a86074e1e7947e764919af728ed68c60
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-11 15:06:17 -0300

    Assert com FluentAssertion

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,6 +1,6 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
-using CashFlow.Communication.Requests;
 using CommomTestUtilities.Requests;
+using FluentAssertions;
 
 namespace Validators.Tests.Expenses.Register;
 
@@ -17,6 +17,6 @@ public class RegisterExpenseValidatorTests
         var result = validator.Validate(request);
 
         //Assert
-        Assert.True(result.IsValid);
+        result.IsValid.Should().BeTrue();
     }   
 }
