cfb4e6d4ca86eebb9d041d892b541f65b5f32896
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-11 16:22:20 -0300

    Criando um teste para um erro

--- tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Exception;
 using CommomTestUtilities.Requests;
 using FluentAssertions;
 
@@ -19,4 +20,17 @@ public class RegisterExpenseValidatorTests
         //Assert
         result.IsValid.Should().BeTrue();
     }   
+
+    [Fact]
+    public void Error_Title_Empty()
+    {
+        var validator = new RegisterExpenseValidator();
+        var request = RequestRegisterExpenseJsonBuilder.Build();
+        request.Title = string.Empty;
+
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErrorMessages.TITLE)); 
+    }
 }
