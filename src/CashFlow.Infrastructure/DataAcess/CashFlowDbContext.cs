4bc6155c806f6d73dd8aa7bc5e636f69bb3da93a
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-15 15:55:57 -0300

    MySQL

---
+++ src/CashFlow.Infrastructure/DataAcess/CashFlowDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAcess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; } // ele que faz a conexão com o banco de dados
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=Senha123!";
+
+        var version = new Version(8, 0, 46);
+        var serverVersion = new MySqlServerVersion(version);
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
