9b4f4a485befc21fd255f8f830b83649170ada06
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-23 15:42:18 -0300

    Inserindo Delete

--- src/CashFlow.Application/DependencyInjectionExtension.cs
+++ src/CashFlow.Application/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using CashFlow.Application.AutoMapper;
+using CashFlow.Application.UseCases.Expenses.Delete;
 using CashFlow.Application.UseCases.Expenses.GetAll;
 using CashFlow.Application.UseCases.Expenses.GetById;
 using CashFlow.Application.UseCases.Expenses.Register;
@@ -27,5 +28,6 @@ public static class DependencyInjectionExtension
         services.AddScoped<IRegisterExpenseUseCase, RegisterExpenseUseCase>();
         services.AddScoped<IGetAllExpensesUseCase, GetAllExpensesUseCase>();
         services.AddScoped<IGetExpenseByIdUseCase, GetExpenseByIdUseCase>();
+        services.AddScoped<IDeleteExpenseUseCase, DeleteExpenseUseCase>();
     }
 }
---
+++ src/CashFlow.Application/UseCases/Expenses/Delete/IDeleteExpenseUseCase.cs
@@ -0,0 +1,8 @@
+﻿using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Delete;
+
+public interface IDeleteExpenseUseCase
+{
+    Task Execute(long id);
+}
