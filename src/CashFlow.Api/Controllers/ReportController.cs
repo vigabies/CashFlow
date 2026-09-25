391bb3ae9321bf5ed7ca42e311f7eaa267c03310
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-25 11:44:49 -0300

    Header

--- src/CashFlow.Api/Controllers/ReportController.cs
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -1,5 +1,4 @@
 ﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
-using CashFlow.Communication.Requests;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
@@ -17,7 +16,7 @@ public class ReportController : ControllerBase
         [FromServices] IGenerateExpensesReportExcelUseCase useCase,
         [FromHeader] DateOnly month)
     {
-        byte[] file = new byte[1];
+        byte[] file = await useCase.Execute(month);
 
         if(file.Length > 0)
             return File(file, MediaTypeNames.Application.Octet, "report.xlsx");
--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -1,9 +1,37 @@
-﻿namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
+﻿using CashFlow.Domain.Reports;
+using ClosedXML.Excel;
+
+namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
 
 public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
 {
     public async Task<byte[]> Execute(DateOnly month)
     {
+        var workbook = new XLWorkbook();
+
+        workbook.Style.Font.FontSize = 12;
+        workbook.Style.Font.FontName = "Calibri";
+        workbook.Author = "Gabriela Silva";
+        
+        var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
+
+        InsertHeader(worksheet);
+    }
+
+    private void InsertHeader(IXLWorksheet worksheet)
+    {
+        worksheet.Cell("A1").Value = ResourceReportGenerationMessages.TITLE;
+        worksheet.Cell("B1").Value = ResourceReportGenerationMessages.DATE;
+        worksheet.Cell("C1").Value = ResourceReportGenerationMessages.PAYMENT_TYPE;
+        worksheet.Cell("D1").Value = ResourceReportGenerationMessages.AMOUNT;
+        worksheet.Cell("E1").Value = ResourceReportGenerationMessages.DESCRIPTION;
+
+        worksheet.Cells("A1:E1").Style.Font.Bold = true;
+        worksheet.Cells("A1:E1").Style.Fill.BackgroundColor = XLColor.FromHtml("#f5c7ff");
+
+        worksheet.Cells("A1:C1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+        worksheet.Cells("D1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Right);
+        worksheet.Cells("E1").Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
 
     }
 }
