cc2d428d6b34363d89013a0a915c13209244e78f
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-25 09:01:08 -0300

    Criando planilha e configurando fontes

--- src/CashFlow.Api/Controllers/ReportController.cs
+++ src/CashFlow.Api/Controllers/ReportController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
 namespace CashFlow.Api.Controllers;
@@ -11,7 +13,9 @@ public class ReportController : ControllerBase
     [HttpGet("excel")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> GetExcel()
+    public async Task<IActionResult> GetExcel(
+        [FromServices] IGenerateExpensesReportExcelUseCase useCase,
+        [FromHeader] DateOnly month)
     {
         byte[] file = new byte[1];
 
---
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -0,0 +1,9 @@
+﻿namespace CashFlow.Application.UseCases.Expenses.Reports.Excel;
+
+public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUseCase
+{
+    public async Task<byte[]> Execute(DateOnly month)
+    {
+
+    }
+}
