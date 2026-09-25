2990d2db9e31f3e0db06cf563bd020c6fe98b37e
Gabriela Silva <vigabiesdivision@gmail.com>
2026-09-25 11:50:34 -0300

    Stream e teste no arquivo

--- src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCase.cs
@@ -16,6 +16,11 @@ public class GenerateExpensesReportExcelUseCase : IGenerateExpensesReportExcelUs
         var worksheet = workbook.Worksheets.Add(month.ToString("Y"));
 
         InsertHeader(worksheet);
+
+        var file = new MemoryStream();
+        workbook.SaveAs(file);
+
+        return file.ToArray();
     }
 
     private void InsertHeader(IXLWorksheet worksheet)
